a2178be56faeed0a571c95d8b381d25ebac6f17c
rye <rmcleod@gmail.com>
2016-01-20 18:58:55 -0600

    Update 1

--- GalaxyGenerator2/GalaxyGenerator2.py
+++ GalaxyGenerator2/GalaxyGenerator2.py
@@ -1,44 +1,71 @@
 import random
 
-class Galaxy(self, size): # number of systems in galaxy
-    def __init__(self):
+class Galaxy: # number of systems in galaxy
+    def __init__(self,size):
+        self.systems = []
         self.size = size
-        pass
+        for i in range(0,self.size):
+            self.systems.append(StarSystem(i))
 
-class StarSystem(self):
-    def __init__(self):
+    def printfullsystem(self):
+        for j in range(0,self.size):
+            print('-' * 30)
+            print(map.systems[j].starclass)
+            print(map.systems[j].index)
+            if map.systems[j].explored:
+                print("Explored")
+                for i in range(0,int(map.systems[j].bodies)):
+                    print("A", map.systems[j].orbitals[i].orb[0],map.systems[j].orbitals[i].orb[2], map.systems[j].orbitals[i].orb[1], map.systems[j].orbitals[i].orb[3])
+            else:
+                print("Unexplored")       
+
+    def exploresystem(self):
+        u = input("What system index?")
+        map.systems[int(u)].explored = True
+
+class StarSystem:
+    def __init__(self,planetindex):
         self.starclass = CreateStar()      # create one random star
-        
-        pass
+        self.bodies = random.randint(0,12)   # create random number of bodies orbiting
+        self.orbitals = []
+        self.ideal = 1
+        self.explored = False
+        self.index = planetindex
+        for x in range(0,self.bodies):
+            self.orbitals.append(Orbital())
 
+class Orbital:
+    def __init__(self):
+        PLANET_TYPES = ['barren',
+                        'ice',
+                        'lava',
+                        'ocean',
+                        'terran',
+                        'gas',
+                        'asteroid']
 
-def CreateOrbitals():  
-     
-    # planet type, size, temperature, biomass, atmosphere
-    
-    PLANET_TYPES = ['Barren',
-                    'Ice',
-                    'Lava',
-                    'Ocean',
-                    'Terran',
-                    'Gas',
-                    'Asteroid Belt']
+        PLANET_SIZES = ['tiny',
+                        'small',
+                        'average',
+                        'large',
+                        'huge']
 
-    PLANET_SIZES = ['Tiny',
-                    'Small',
-                    'Average',
-                    'Large',
-                    'Huge']
+        PLANET_TEMPS = ['frozen',
+                        'cool',
+                        'temperate',
+                        'warm',
+                        'scorching']
 
-    PLANET_TEMPS = ['Frozen',
-                    'Cool',
-                    'Temperate',
-                    'Warm',
-                    'Scorching']
-
-    
-
-
+        self.orb = []
+        self.orb.append(random.choice(PLANET_SIZES))
+        self.orb.append(random.choice(PLANET_TYPES))
+        self.orb.append(random.choice(PLANET_TEMPS))
+        if self.orb[1] == 'asteroid':
+            self.orb.append('belt')
+        else:
+            self.orb.append('planet')
+            
+       
 
 
 def CreateStar():   # choose a star type based on various probablities, return star type (txt)
@@ -59,4 +86,19 @@ def CreateStar():   # choose a star type based on various probablities, return s
         star = "Class M"
     
     return star
+
+x = int(input("Number of Stars: "))
+map = Galaxy(x)
+
+
+running = True
+
+while running:
+    y = input("Now what? ")
+    if y == 'print':
+        map.printfullsystem()
+    elif y == 'x':
+        map.exploresystem()    
+    elif y == 'q':
+        running = False
 
